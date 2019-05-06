29ff969f878682ca44f3f75a160d366314d4b5b0
Brayden Steven Gray <845858218@bun2-206-02.local>
2019-05-07 11:51:27 +1200

    class finished

--- Week2/Tuesday/js/script.js
+++ Week2/Tuesday/js/script.js
@@ -16,19 +16,50 @@ document.write("<ul>");
     };
 document.write("</ul>");
 
-var days = ["Monday", "Tuesday", "Wednesday"]
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 var tasks = [
   ["Cleaning", "Shopping"],
-  ["Homework", "something else"],
-  ["Work", "Gym"]
+  ["Homework", "something else", "Shopping"],
+  ["Work", "Gym"],
+  [],
+  ["Cleaning", "Go to the Pub"]
 ]
 console.log(days);
 console.log(tasks);
 
 document.write("<h2>Daily Tasks</h2>");
+
 document.write("<ul>");
     for (var i = 0; i < days.length; i++) {
-      document.write("<li>" + days[i] + "</li>");
-      console.log(tasks[i]);
+      document.write("<li>" + days[i]);
+          document.write("<ul>")
+
+              if (tasks[i].length === 0) {
+                    document.write("<li>Well done, you have no tasks today</li>")
+              }  else {
+                    for (var a = 0; a < tasks[i].length; a++) {
+                        document.write("<li>" +tasks [i] [a] + "</li>")
+                    }
+              }
+          document.write("</ul>")
+      document.write("</li>");
     }
-    document.write("</ul>");
+document.write("</ul>");
+
+
+
+
+
+
+
+
+
+
+//function
+
+function namePrompt(){
+  console.log("This is coming from namePrompt");
+  var name = prompt("What is your name?");
+  console.log("Hello " + name);
+  document.getElementById("welcoming").innerHTML += "<h4>Hello " + name + ", welcome to the site</h4>";
+};
