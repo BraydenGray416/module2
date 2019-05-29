ee79059b409426d5b5ca72d10296cefcd565a181
Brayden Steven Gray <845858218@bun2-206-02.local>
2019-05-30 11:45:24 +1200

    started slideshow

---
+++ week5/Thursday/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('.slideContainer').remove('.slide1')
+  $('.slideContainer').append('<div class="slide slide1 active"><img src="assets/spiderman1.jpg" alt=""></div>')
+
+
+});
