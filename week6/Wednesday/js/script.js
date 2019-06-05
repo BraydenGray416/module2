85460dacdcd94e6f0f0e57113a747bd0595ce951
Brayden Steven Gray <845858218@bun2-206-02.local>
2019-06-06 08:33:06 +1200

    Wednesday week 6 finished

---
+++ week6/Wednesday/js/script.js
@@ -0,0 +1,18 @@
+// $(document).ready(function(){
+//   $(".owl-carousel").owlCarousel({
+//     items: 1,
+//     loop: true,
+//     autoplay: true,
+//     center: true,
+//     autoplayTimeout: 2000,
+//     autoplaySpeed: 1000
+//   });
+// });
+
+
+ScrollReveal({ reset: true });
+
+
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.tagline', { delay: 500 });
+ScrollReveal().reveal('.punchline', { delay: 2000 });
