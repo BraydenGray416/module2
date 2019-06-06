1d53635dd9e2a3a573aa73fb0eedf638482fb6e0
Brayden Steven Gray <845858218@bun2-206-02.local>
2019-06-07 11:16:37 +1200

    All js working still needs Data Entry for pokemon filter

--- formative2.2/js/script.js
+++ formative2.2/js/script.js
@@ -1,508 +1,573 @@
 var pokemon = [
   {
-    id: 001,
+    id: 1,
     name: "Bulbasaur",
-    sprite: "250px-001Bulbasaur.png"
+    sprite: "250px-001Bulbasaur.png",
+    type: ["Grass", "Poison"],
+    bio: "Bulbasaur (Japanese: フシギダネ Fushigidane) is a dual-type Grass/Poison Pokémon introduced in Generation I. It evolves into Ivysaur starting at level 16, which evolves into Venusaur starting at level 32. Along with Charmander and Squirtle, Bulbasaur is one of three starter Pokémon of Kanto available at the beginning of Pokémon Red, Green, Blue, FireRed, and LeafGreen."
   },
   {
-    id: 002,
+    id: 2,
     name: "Ivysaur",
-    sprite: "250px-002Ivysaur.png"
+    sprite: "250px-002Ivysaur.png",
+    type: ["Grass", "Poison"]
   },
   {
-    id: 003,
+    id: 3,
     name: "Venusaur",
-    sprite: "250px-003Venusaur.png"
+    sprite: "250px-003Venusaur.png",
+    type: ["Grass", "Poison"]
   },
   {
-    id: 004,
+    id: 4,
     name: "Charmander",
-    sprite: "250px-004Charmander.png"
+    sprite: "250px-004Charmander.png",
+    type: ["Fire"]
   },
   {
-    id: 005,
+    id: 5,
     name: "Charmeleon",
-    sprite: "250px-005Charmeleon.png"
+    sprite: "250px-005Charmeleon.png",
+    type: ["Fire"]
   },
   {
-    id: 006,
+    id: 6,
     name: "Charizard",
-    sprite: "250px-006Charizard.png"
+    sprite: "250px-006Charizard.png",
+    type: ["Fire", "Flying"]
   },
   {
-    id: 007,
+    id: 7,
     name: "Squirtle",
-    sprite: "250px-007Squirtle.png"
+    sprite: "250px-007Squirtle.png",
+    type: ["Water"]
   },
   {
-    id: 008,
+    id: 8,
     name: "Wartortle",
-    sprite: "250px-008Wartortle.png"
+    sprite: "250px-008Wartortle.png",
+    type: ["Water"]
   },
   {
-    id: 009,
+    id: 9,
     name: "Blastoise",
-    sprite: "250px-009Blastoise.png"
+    sprite: "250px-009Blastoise.png",
+    type: ["Water"]
   },
   {
-    id: 010,
+    id: 10,
     name: "Caterpie",
-    sprite: "250px-010Caterpie.png"
+    sprite: "250px-010Caterpie.png",
+    type: ["Bug"]
   },
   {
-    id: 011,
+    id: 11,
     name: "Metapod",
-    sprite: "250px-011Metapod.png"
+    sprite: "250px-011Metapod.png",
+    type: ["Bug"]
   },
   {
-    id: 012,
+    id: 12,
     name: "Butterfree",
-    sprite: "250px-012Butterfree.png"
+    sprite: "250px-012Butterfree.png",
+    type: ["Bug", "Flying"]
   },
   {
-    id: 013,
+    id: 13,
     name: "Weedle",
-    sprite: "250px-013Weedle.png"
+    sprite: "250px-013Weedle.png",
+    type: ["Bug", "Poison"]
   },
   {
-    id: 014,
+    id: 14,
     name: "Kakuna",
-    sprite: "250px-014Kakuna.png"
+    sprite: "250px-014Kakuna.png",
+    type: ["Bug", "Poison"]
   },
   {
-    id: 015,
+    id: 15,
     name: "Beedrill",
-    sprite: "250px-015Beedrill.png"
+    sprite: "250px-015Beedrill.png",
+    type: ["Bug", "Poison"]
   },
   {
-    id: 016,
+    id: 16,
     name: "Pidgey",
-    sprite: "250px-016Pidgey.png"
+    sprite: "250px-016Pidgey.png",
+    type: ["Normal", "Flying"]
   },
   {
-    id: 017,
+    id: 17,
     name: "Pidgeotto",
-    sprite: "250px-017Pidgeotto.png"
+    sprite: "250px-017Pidgeotto.png",
+    type: ["Normal", "Flying"]
   },
   {
-    id: 018,
+    id: 18,
     name: "Pidgeot",
-    sprite: "250px-018Pidgeot.png"
+    sprite: "250px-018Pidgeot.png",
+    type: ["Normal", "Flying"]
   },
   {
-    id: 019,
+    id: 19,
     name: "Rattata",
-    sprite: "250px-019Rattata.png"
+    sprite: "250px-019Rattata.png",
+    type: ["Normal"]
   },
   {
-    id: 020,
+    id: 20,
     name: "Raticate",
-    sprite: "250px-020Raticate.png"
+    sprite: "250px-020Raticate.png",
+    type: ["Normal"]
   },
   {
-    id: 021,
+    id: 21,
     name: "Spearow",
-    sprite: "250px-021Spearow.png"
+    sprite: "250px-021Spearow.png",
+    type: ["Normal", "Flying"]
   },
   {
-    id: 022,
+    id: 22,
     name: "Fearow",
-    sprite: "250px-022Fearow.png"
+    sprite: "250px-022Fearow.png",
+    type: ["Normal", "Flying"]
   },
   {
-    id: 023,
+    id: 23,
     name: "Ekans",
-    sprite: "250px-023Ekans.png"
+    sprite: "250px-023Ekans.png",
+    type: ["Poison"]
   },
   {
-    id: 024,
+    id: 24,
     name: "Arbok",
-    sprite: "250px-024Arbok.png"
+    sprite: "250px-024Arbok.png",
+    type: ["Poison"]
   },
   {
-    id: 025,
+    id: 25,
     name: "Pikachu",
-    sprite: "250px-025Pikachu.png"
+    sprite: "250px-025Pikachu.png",
+    type: ["Electric"]
   },
   {
-    id: 026,
+    id: 26,
     name: "Raichu",
-    sprite: "250px-026Raichu.png"
+    sprite: "250px-026Raichu.png",
+    type: ["Electric"]
   },
   {
-    id: 027,
+    id: 27,
     name: "Sandshrew",
-    sprite: "250px-027Sandshrew.png"
+    sprite: "250px-027Sandshrew.png",
+    type: ["Ground"]
   },
   {
-    id: 028,
+    id: 28,
     name: "Sandslash",
-    sprite: "250px-028Sandslash.png"
+    sprite: "250px-028Sandslash.png",
+    type: ["Ground"]
   },
   {
-    id: 029,
+    id: 29,
     name: "Nidoran",
-    sprite: "250px-029Nidoran.png"
+    sprite: "250px-029Nidoran.png",
+    type: ["Poison"]
   },
   {
-    id: 030,
+    id: 30,
     name: "Nidorina",
-    sprite: "250px-030Nidorina.png"
+    sprite: "250px-030Nidorina.png",
+    type: ["Poison"]
   },
   {
-    id: 031,
+    id: 31,
     name: "Nidoqueen",
-    sprite: "250px-031Nidoqueen.png"
+    sprite: "250px-031Nidoqueen.png",
+    type: ["Poison", "Ground"]
   },
   {
-    id: 032,
+    id: 32,
     name: "Nidoran",
-    sprite: "250px-032Nidoran.png"
+    sprite: "250px-032Nidoran.png",
+    type: ["Poison"]
   },
   {
-    id: 033,
+    id: 33,
     name: "Nidorino",
-    sprite: "250px-033Nidorino.png"
+    sprite: "250px-033Nidorino.png",
+    type: ["Poison"]
   },
   {
-    id: 034,
+    id: 34,
     name: "Nidoking",
-    sprite: "250px-034Nidoking.png"
+    sprite: "250px-034Nidoking.png",
+    type: ["Poison", "Ground"]
   },
   {
-    id: 035,
+    id: 35,
     name: "Clefairy",
-    sprite: "250px-035Clefairy.png"
+    sprite: "250px-035Clefairy.png",
+    type: ["Fairy"]
   },
   {
-    id: 036,
+    id: 36,
     name: "Clefable",
-    sprite: "250px-036Clefable.png"
+    sprite: "250px-036Clefable.png",
+    type: ["Fairy"]
   },
   {
-    id: 037,
+    id: 37,
     name: "Vulpix",
-    sprite: "250px-037Vulpix.png"
+    sprite: "250px-037Vulpix.png",
+    type: ["Fire"]
   },
   {
-    id: 038,
+    id: 38,
     name: "Ninetales",
-    sprite: "250px-038Ninetales.png"
+    sprite: "250px-038Ninetales.png",
+    type: ["Fire"]
   },
   {
-    id: 039,
+    id: 39,
     name: "Jigglypuff",
-    sprite: "250px-039Jigglypuff.png"
+    sprite: "250px-039Jigglypuff.png",
+    type: ["Normal", "Fairy"]
   },
   {
-    id: 040,
+    id: 40,
     name: "Wigglytuff",
-    sprite: "250px-040Wigglytuff.png"
+    sprite: "250px-040Wigglytuff.png",
+    type: ["Normal", "Fairy"]
   },
   {
-    id: 041,
+    id: 41,
     name: "Zubat",
-    sprite: "250px-041Zubat.png"
+    sprite: "250px-041Zubat.png",
+    type: ["Poison", "Flying"]
   },
   {
-    id: 042,
+    id: 42,
     name: "Golbat",
-    sprite: "250px-042Golbat.png"
+    sprite: "250px-042Golbat.png",
+    type: ["Poison", "Flying"]
   },
   {
-    id: 043,
+    id: 43,
     name: "Oddish",
-    sprite: "250px-043Oddish.png"
+    sprite: "250px-043Oddish.png",
+    type: ["Grass", "Poison"]
   },
   {
-    id: 044,
+    id: 44,
     name: "Gloom",
-    sprite: "250px-044Gloom.png"
+    sprite: "250px-044Gloom.png",
+    type: ["Grass", "Poison"]
   },
   {
-    id: 045,
+    id: 45,
     name: "Vileplume",
-    sprite: "250px-045Vileplume.png"
+    sprite: "250px-045Vileplume.png",
+    type: ["Grass", "Poison"]
   },
   {
-    id: 046,
+    id: 46,
     name: "Paras",
-    sprite: "250px-046Paras.png"
+    sprite: "250px-046Paras.png",
+    type: ["Bug", "Grass"]
   },
   {
-    id: 047,
+    id: 47,
     name: "Parasect",
-    sprite: "250px-047Parasect.png"
+    sprite: "250px-047Parasect.png",
+    type: ["Bug", "Grass"]
   },
   {
-    id: 048,
+    id: 48,
     name: "Venonat",
-    sprite: "250px-048Venonat.png"
+    sprite: "250px-048Venonat.png",
+    type: ["Bug", "Poison"]
   },
   {
-    id: 049,
+    id: 49,
     name: "Venomoth",
-    sprite: "250px-049Venomoth.png"
+    sprite: "250px-049Venomoth.png",
+    type: ["Bug", "Poison"]
   },
   {
-    id: 050,
+    id: 50,
     name: "Diglett",
-    sprite: "250px-050Diglett.png"
+    sprite: "250px-050Diglett.png",
+    type: ["Ground"]
   },
   {
-    id: 051,
+    id: 51,
     name: "Dugtrio",
-    sprite: "250px-051Dugtrio.png"
+    sprite: "250px-051Dugtrio.png",
+    type: ["Ground"]
   },
   {
-    id: 052,
+    id: 52,
     name: "Meowth",
-    sprite: "250px-052Meowth.png"
+    sprite: "250px-052Meowth.png",
+    type: ["Normal"]
   },
   {
-    id: 053,
+    id: 53,
     name: "Persian",
-    sprite: "250px-053Persian.png"
+    sprite: "250px-053Persian.png",
+    type: ["Normal"]
   },
   {
-    id: 054,
+    id: 54,
     name: "Psyduck",
-    sprite: "250px-054Psyduck.png"
+    sprite: "250px-054Psyduck.png",
+    type: ["Water"]
   },
   {
-    id: 055,
+    id: 55,
     name: "Golduck",
-    sprite: "250px-055Golduck.png"
+    sprite: "250px-055Golduck.png",
+    type: ["Water"]
   },
   {
-    id: 056,
+    id: 56,
     name: "Mankey",
     sprite: "250px-056Mankey.png"
   },
   {
-    id: 057,
+    id: 57,
     name: "Primeape",
     sprite: "250px-057Primeape.png"
   },
   {
-    id: 058,
+    id: 58,
     name: "Growlithe",
     sprite: "250px-058Growlithe.png"
   },
   {
-    id: 059,
+    id: 59,
     name: "Arcanine",
     sprite: "250px-059Arcanine.png"
   },
   {
-    id: 060,
+    id: 60,
     name: "Poliwag",
-    sprite: "250px-060Poliwag.png"
+    sprite: "250px-060Poliwag.png",
+    type: ["Water"]
   },
   {
-    id: 061,
+    id: 61,
     name: "Poliwhirl",
-    sprite: "250px-061Poliwhirl.png"
+    sprite: "250px-061Poliwhirl.png",
+    type: ["Water"]
   },
   {
-    id: 062,
+    id: 62,
     name: "Poliwrath",
     sprite: "250px-062Poliwrath.png"
   },
   {
-    id: 063,
+    id: 63,
     name: "Abra",
     sprite: "250px-063Abra.png"
   },
   {
-    id: 064,
+    id: 64,
     name: "Kadabra",
     sprite: "250px-064Kadabra.png"
   },
   {
-    id: 065,
+    id: 65,
     name: "Alakazam",
     sprite: "250px-065Alakazam.png"
   },
   {
-    id: 066,
+    id: 66,
     name: "Machop",
     sprite: "250px-066Machop.png"
   },
   {
-    id: 067,
+    id: 67,
     name: "Machoke",
     sprite: "250px-067Machoke.png"
   },
   {
-    id: 068,
+    id: 68,
     name: "Machamp",
     sprite: "250px-068Machamp.png"
   },
   {
-    id: 069,
+    id: 69,
     name: "Bellsprout",
     sprite: "250px-069Bellsprout.png"
   },
   {
-    id: 070,
+    id: 70,
     name: "Weepinbell",
     sprite: "250px-070Weepinbell.png"
   },
   {
-    id: 071,
+    id: 71,
     name: "Victreebel",
     sprite: "250px-071Victreebel.png"
   },
   {
-    id: 072,
+    id: 72,
     name: "Tentacool",
     sprite: "250px-072Tentacool.png"
   },
   {
-    id: 073,
+    id: 73,
     name: "Tentacruel",
     sprite: "250px-073Tentacruel.png"
   },
   {
-    id: 074,
+    id: 74,
     name: "Geodude",
     sprite: "250px-074Geodude.png"
   },
   {
-    id: 075,
+    id: 75,
     name: "Graveler",
     sprite: "250px-075Graveler.png"
   },
   {
-    id: 076,
+    id: 76,
     name: "Golem",
     sprite: "250px-076Golem.png"
   },
   {
-    id: 077,
+    id: 77,
     name: "Ponyta",
     sprite: "250px-077Ponyta.png"
   },
   {
-    id: 078,
+    id: 78,
     name: "Rapidash",
     sprite: "250px-078Rapidash.png"
   },
   {
-    id: 079,
+    id: 79,
     name: "Slowpoke",
-    sprite: "250px-079Slowpoke.png"
+    sprite: "250px-079Slowpoke.png",
+    type: ["Water"]
   },
   {
-    id: 080,
+    id: 80,
     name: "Slowbro",
     sprite: "250px-080Slowbro.png"
   },
   {
-    id: 081,
+    id: 81,
     name: "Magnemite",
     sprite: "250px-081Magnemite.png"
   },
   {
-    id: 082,
+    id: 82,
     name: "Magneton",
     sprite: "250px-082Magneton.png"
   },
   {
-    id: 083,
+    id: 83,
     name: "Farfetch'd",
     sprite: "250px-083Farfetch'd.png"
   },
   {
-    id: 084,
+    id: 84,
     name: "Doduo",
     sprite: "250px-084Doduo.png"
   },
   {
-    id: 085,
+    id: 85,
     name: "Dodrio",
     sprite: "250px-085Dodrio.png"
   },
   {
-    id: 086,
+    id: 86,
     name: "Seel",
-    sprite: "250px-086Seel.png"
+    sprite: "250px-086Seel.png",
+    type: ["Water"]
   },
   {
-    id: 087,
+    id: 87,
     name: "Dewgong",
     sprite: "250px-087Dewgong.png"
   },
   {
-    id: 088,
+    id: 88,
     name: "Grimer",
     sprite: "250px-088Grimer.png"
   },
   {
-    id: 089,
+    id: 89,
     name: "Muk",
     sprite: "250px-089Muk.png"
   },
   {
-    id: 090,
+    id: 90,
     name: "Shellder",
-    sprite: "250px-090Shellder.png"
+    sprite: "250px-090Shellder.png",
+    type: ["Water"]
   },
   {
-    id: 091,
+    id: 91,
     name: "Cloyster",
     sprite: "250px-091Cloyster.png"
   },
   {
-    id: 092,
+    id: 92,
     name: "Gastly",
     sprite: "250px-092Gastly.png"
   },
   {
-    id: 093,
+    id: 93,
     name: "Haunter",
     sprite: "250px-093Haunter.png"
   },
   {
-    id: 094,
+    id: 94,
     name: "Gengar",
     sprite: "250px-094Gengar.png"
   },
   {
-    id: 095,
+    id: 95,
     name: "Onix",
     sprite: "250px-095Onix.png"
   },
   {
-    id: 096,
+    id: 96,
     name: "Drowzee",
     sprite: "250px-096Drowzee.png"
   },
   {
-    id: 097,
+    id: 97,
     name: "Hypno",
     sprite: "250px-097Hypno.png"
   },
   {
-    id: 098,
+    id: 98,
     name: "Krabby",
-    sprite: "250px-098Krabby.png"
+    sprite: "250px-098Krabby.png",
+    type: ["Water"]
   },
   {
-    id: 099,
+    id: 99,
     name: "Kingler",
-    sprite: "250px-099Kingler.png"
+    sprite: "250px-099Kingler.png",
+    type: ["Water"]
   },
   {
     id: 100,
     name: "Voltorb",
-    sprite: "250px-100Voltorb.png"
+    sprite: "250px-100Voltorb.png",
+    type: ["Electric"]
   },
   {
     id: 101,
     name: "Electrode",
-    sprite: "250px-101Electrode.png"
+    sprite: "250px-101Electrode.png",
+    type: ["Electric"]
   },
   {
     id: 102,
@@ -577,22 +642,26 @@ var pokemon = [
   {
     id: 116,
     name: "Horsea",
-    sprite: "250px-116Horsea.png"
+    sprite: "250px-116Horsea.png",
+    type: ["Water"]
   },
   {
     id: 117,
     name: "Seadra",
-    sprite: "250px-117Seadra.png"
+    sprite: "250px-117Seadra.png",
+    type: ["Water"]
   },
   {
     id: 118,
     name: "Goldeen",
-    sprite: "250px-118Goldeen.png"
+    sprite: "250px-118Goldeen.png",
+    type: ["Water"]
   },
   {
     id: 119,
     name: "Seaking",
-    sprite: "250px-119Seaking.png"
+    sprite: "250px-119Seaking.png",
+    type: ["Water"]
   },
   {
     id: 120,
@@ -642,12 +711,14 @@ var pokemon = [
   {
     id: 129,
     name: "Magikarp",
-    sprite: "250px-129Magikarp.png"
+    sprite: "250px-129Magikarp.png",
+    type: ["Water"]
   },
   {
     id: 130,
     name: "Gyarados",
-    sprite: "250px-130Gyarados.png"
+    sprite: "250px-130Gyarados.png",
+    type: ["Water"]
   },
   {
     id: 131,
@@ -667,12 +738,14 @@ var pokemon = [
   {
     id: 134,
     name: "Vaporeon",
-    sprite: "250px-134Vaporeon.png"
+    sprite: "250px-134Vaporeon.png",
+    type: ["Water"]
   },
   {
     id: 135,
     name: "Jolteon",
-    sprite: "250px-135Jolteon.png"
+    sprite: "250px-135Jolteon.png",
+    type: ["Electric"]
   },
   {
     id: 136,
@@ -754,7 +827,6 @@ var pokemon = [
     name: "Mew",
     sprite: "250px-151Mew.png"
   }
-
 ];
 var pageContainer = document.getElementById('pageContainer');
 
@@ -768,9 +840,9 @@ pageContainer.innerHTML += '<div id="pokemonList" class="row border border-dange
     var pokeList = pokemon[i];
 
     var pokeCard = '<div class="col-12 col-sm-6 col-md-2 mb-3 mt-3 text-center">';
-            pokeCard += '<div class="movieThumb movieThumb2 card h-100 border-dark">';
+            pokeCard += '<div class="pokeThumb pokeThumb2 card h-100 border-dark" onclick="showMorePoke('+pokeList.id+');">';
                 pokeCard += '<div class="card-body">'
-                    pokeCard += '<img src="assets/'+pokeList.sprite+'" class="card-img-top" alt""';
+                    pokeCard += '<img src="assets/'+pokeList.sprite+'" class="card-img-top" alt"">';
                     pokeCard += '<h5 class="card-title">'+pokeList.name+'</h5>'
                 pokeCard += '</div>';
             pokeCard += '</div>';
@@ -781,5 +853,65 @@ pageContainer.innerHTML += '<div id="pokemonList" class="row border border-dange
 
 
 }
+
+function showMorePoke(pokeNumber){
+  var singlePoke;
+  console.log("You have clicked on number ");
+  for (var i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].id === pokeNumber) {
+      singlePoke = pokemon[i]
+      break;
+    }
+  }
+  document.getElementById('pokeImage').src = 'assets/'+singlePoke.sprite;
+  document.getElementById('pokeName').innerText = singlePoke.name;
+  document.getElementById('pokeId').innerText = singlePoke.id;
+  document.getElementById('pokeBio').innerText = singlePoke.bio;
+  document.getElementById('pokeType').innerHTML = '';
+  var typeColour;
+  for (var i = 0; i < singlePoke.type.length; i++) {
+    // var typeColour = getTypeColour(singlePoke.type[i]);
+
+      document.getElementById('pokeType').innerHTML += '<span class="badge badge-pill badge-danger">'+singlePoke.type[i]+'</span>';
+  }
+
+  document.getElementById('pokePopUp').style.display= "flex";
+  document.body.style.overflow = "hidden";
+
+  document.getElementById('close').onclick = function(){
+  document.getElementById('pokePopUp').style.display= "none";
+  document.body.style.overflow = "scroll";
+}
+
+}
+
+function typeClick (pokemonType){
+  document.getElementById('pokemonList').innerHTML = '';
+  for (var a = 0; a < pokemon.length; a++) {
+    var pokeList = pokemon[a];
+    if (pokeList.type.includes(pokemonType)) {
+        document.getElementById('pokemonList').innerHTML +=
+            pokeCard = '<div class="col-12 col-sm-6 col-md-2 mb-3 mt-3 text-center">';
+                pokeCard += '<div class="pokeThumb pokeThumb2 card h-100 border-dark" onclick="showMorePoke('+pokeList.id+')">';
+                    pokeCard += '<div class="card-body">'
+                        pokeCard += '<img src="assets/'+pokeList.sprite+'" class="card-img-top" alt"">';
+                        pokeCard += '<h5 class="card-title">'+pokeList.name+'</h5>'
+                    pokeCard += '</div>';
+                pokeCard += '</div>';
+            pokeCard += '</div>';
+
+            document.getElementById('pokemonList').innerHTML += pokeCard;
+    }
+  }
+
+}
+
+function showAll(){
+  document.getElementById('pokemonList').innerHTML = '';
+  showPokemon();
+}
+
+
+
 
 showPokemon();
