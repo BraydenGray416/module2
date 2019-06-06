64fee89331d5489f63815c8237e56958d8c7d28d
Steve Gray <brayden.gray98@gmail.com>
2019-06-07 05:46:03 +1200

    all pokemon assets added

--- formative2.2/js/script.js
+++ formative2.2/js/script.js
@@ -119,6 +119,642 @@ var pokemon = [
     name: "Arbok",
     sprite: "250px-024Arbok.png"
   },
+  {
+    id: 025,
+    name: "Pikachu",
+    sprite: "250px-025Pikachu.png"
+  },
+  {
+    id: 026,
+    name: "Raichu",
+    sprite: "250px-026Raichu.png"
+  },
+  {
+    id: 027,
+    name: "Sandshrew",
+    sprite: "250px-027Sandshrew.png"
+  },
+  {
+    id: 028,
+    name: "Sandslash",
+    sprite: "250px-028Sandslash.png"
+  },
+  {
+    id: 029,
+    name: "Nidoran",
+    sprite: "250px-029Nidoran.png"
+  },
+  {
+    id: 030,
+    name: "Nidorina",
+    sprite: "250px-030Nidorina.png"
+  },
+  {
+    id: 031,
+    name: "Nidoqueen",
+    sprite: "250px-031Nidoqueen.png"
+  },
+  {
+    id: 032,
+    name: "Nidoran",
+    sprite: "250px-032Nidoran.png"
+  },
+  {
+    id: 033,
+    name: "Nidorino",
+    sprite: "250px-033Nidorino.png"
+  },
+  {
+    id: 034,
+    name: "Nidoking",
+    sprite: "250px-034Nidoking.png"
+  },
+  {
+    id: 035,
+    name: "Clefairy",
+    sprite: "250px-035Clefairy.png"
+  },
+  {
+    id: 036,
+    name: "Clefable",
+    sprite: "250px-036Clefable.png"
+  },
+  {
+    id: 037,
+    name: "Vulpix",
+    sprite: "250px-037Vulpix.png"
+  },
+  {
+    id: 038,
+    name: "Ninetales",
+    sprite: "250px-038Ninetales.png"
+  },
+  {
+    id: 039,
+    name: "Jigglypuff",
+    sprite: "250px-039Jigglypuff.png"
+  },
+  {
+    id: 040,
+    name: "Wigglytuff",
+    sprite: "250px-040Wigglytuff.png"
+  },
+  {
+    id: 041,
+    name: "Zubat",
+    sprite: "250px-041Zubat.png"
+  },
+  {
+    id: 042,
+    name: "Golbat",
+    sprite: "250px-042Golbat.png"
+  },
+  {
+    id: 043,
+    name: "Oddish",
+    sprite: "250px-043Oddish.png"
+  },
+  {
+    id: 044,
+    name: "Gloom",
+    sprite: "250px-044Gloom.png"
+  },
+  {
+    id: 045,
+    name: "Vileplume",
+    sprite: "250px-045Vileplume.png"
+  },
+  {
+    id: 046,
+    name: "Paras",
+    sprite: "250px-046Paras.png"
+  },
+  {
+    id: 047,
+    name: "Parasect",
+    sprite: "250px-047Parasect.png"
+  },
+  {
+    id: 048,
+    name: "Venonat",
+    sprite: "250px-048Venonat.png"
+  },
+  {
+    id: 049,
+    name: "Venomoth",
+    sprite: "250px-049Venomoth.png"
+  },
+  {
+    id: 050,
+    name: "Diglett",
+    sprite: "250px-050Diglett.png"
+  },
+  {
+    id: 051,
+    name: "Dugtrio",
+    sprite: "250px-051Dugtrio.png"
+  },
+  {
+    id: 052,
+    name: "Meowth",
+    sprite: "250px-052Meowth.png"
+  },
+  {
+    id: 053,
+    name: "Persian",
+    sprite: "250px-053Persian.png"
+  },
+  {
+    id: 054,
+    name: "Psyduck",
+    sprite: "250px-054Psyduck.png"
+  },
+  {
+    id: 055,
+    name: "Golduck",
+    sprite: "250px-055Golduck.png"
+  },
+  {
+    id: 056,
+    name: "Mankey",
+    sprite: "250px-056Mankey.png"
+  },
+  {
+    id: 057,
+    name: "Primeape",
+    sprite: "250px-057Primeape.png"
+  },
+  {
+    id: 058,
+    name: "Growlithe",
+    sprite: "250px-058Growlithe.png"
+  },
+  {
+    id: 059,
+    name: "Arcanine",
+    sprite: "250px-059Arcanine.png"
+  },
+  {
+    id: 060,
+    name: "Poliwag",
+    sprite: "250px-060Poliwag.png"
+  },
+  {
+    id: 061,
+    name: "Poliwhirl",
+    sprite: "250px-061Poliwhirl.png"
+  },
+  {
+    id: 062,
+    name: "Poliwrath",
+    sprite: "250px-062Poliwrath.png"
+  },
+  {
+    id: 063,
+    name: "Abra",
+    sprite: "250px-063Abra.png"
+  },
+  {
+    id: 064,
+    name: "Kadabra",
+    sprite: "250px-064Kadabra.png"
+  },
+  {
+    id: 065,
+    name: "Alakazam",
+    sprite: "250px-065Alakazam.png"
+  },
+  {
+    id: 066,
+    name: "Machop",
+    sprite: "250px-066Machop.png"
+  },
+  {
+    id: 067,
+    name: "Machoke",
+    sprite: "250px-067Machoke.png"
+  },
+  {
+    id: 068,
+    name: "Machamp",
+    sprite: "250px-068Machamp.png"
+  },
+  {
+    id: 069,
+    name: "Bellsprout",
+    sprite: "250px-069Bellsprout.png"
+  },
+  {
+    id: 070,
+    name: "Weepinbell",
+    sprite: "250px-070Weepinbell.png"
+  },
+  {
+    id: 071,
+    name: "Victreebel",
+    sprite: "250px-071Victreebel.png"
+  },
+  {
+    id: 072,
+    name: "Tentacool",
+    sprite: "250px-072Tentacool.png"
+  },
+  {
+    id: 073,
+    name: "Tentacruel",
+    sprite: "250px-073Tentacruel.png"
+  },
+  {
+    id: 074,
+    name: "Geodude",
+    sprite: "250px-074Geodude.png"
+  },
+  {
+    id: 075,
+    name: "Graveler",
+    sprite: "250px-075Graveler.png"
+  },
+  {
+    id: 076,
+    name: "Golem",
+    sprite: "250px-076Golem.png"
+  },
+  {
+    id: 077,
+    name: "Ponyta",
+    sprite: "250px-077Ponyta.png"
+  },
+  {
+    id: 078,
+    name: "Rapidash",
+    sprite: "250px-078Rapidash.png"
+  },
+  {
+    id: 079,
+    name: "Slowpoke",
+    sprite: "250px-079Slowpoke.png"
+  },
+  {
+    id: 080,
+    name: "Slowbro",
+    sprite: "250px-080Slowbro.png"
+  },
+  {
+    id: 081,
+    name: "Magnemite",
+    sprite: "250px-081Magnemite.png"
+  },
+  {
+    id: 082,
+    name: "Magneton",
+    sprite: "250px-082Magneton.png"
+  },
+  {
+    id: 083,
+    name: "Farfetch'd",
+    sprite: "250px-083Farfetch'd.png"
+  },
+  {
+    id: 084,
+    name: "Doduo",
+    sprite: "250px-084Doduo.png"
+  },
+  {
+    id: 085,
+    name: "Dodrio",
+    sprite: "250px-085Dodrio.png"
+  },
+  {
+    id: 086,
+    name: "Seel",
+    sprite: "250px-086Seel.png"
+  },
+  {
+    id: 087,
+    name: "Dewgong",
+    sprite: "250px-087Dewgong.png"
+  },
+  {
+    id: 088,
+    name: "Grimer",
+    sprite: "250px-088Grimer.png"
+  },
+  {
+    id: 089,
+    name: "Muk",
+    sprite: "250px-089Muk.png"
+  },
+  {
+    id: 090,
+    name: "Shellder",
+    sprite: "250px-090Shellder.png"
+  },
+  {
+    id: 091,
+    name: "Cloyster",
+    sprite: "250px-091Cloyster.png"
+  },
+  {
+    id: 092,
+    name: "Gastly",
+    sprite: "250px-092Gastly.png"
+  },
+  {
+    id: 093,
+    name: "Haunter",
+    sprite: "250px-093Haunter.png"
+  },
+  {
+    id: 094,
+    name: "Gengar",
+    sprite: "250px-094Gengar.png"
+  },
+  {
+    id: 095,
+    name: "Onix",
+    sprite: "250px-095Onix.png"
+  },
+  {
+    id: 096,
+    name: "Drowzee",
+    sprite: "250px-096Drowzee.png"
+  },
+  {
+    id: 097,
+    name: "Hypno",
+    sprite: "250px-097Hypno.png"
+  },
+  {
+    id: 098,
+    name: "Krabby",
+    sprite: "250px-098Krabby.png"
+  },
+  {
+    id: 099,
+    name: "Kingler",
+    sprite: "250px-099Kingler.png"
+  },
+  {
+    id: 100,
+    name: "Voltorb",
+    sprite: "250px-100Voltorb.png"
+  },
+  {
+    id: 101,
+    name: "Electrode",
+    sprite: "250px-101Electrode.png"
+  },
+  {
+    id: 102,
+    name: "Exeggcute",
+    sprite: "250px-102Exeggcute.png"
+  },
+  {
+    id: 103,
+    name: "Exeggutor",
+    sprite: "250px-103Exeggutor.png"
+  },
+  {
+    id: 104,
+    name: "Cubone",
+    sprite: "250px-104Cubone.png"
+  },
+  {
+    id: 105,
+    name: "Marowak",
+    sprite: "250px-105Marowak.png"
+  },
+  {
+    id: 106,
+    name: "Hitmonlee",
+    sprite: "250px-106Hitmonlee.png"
+  },
+  {
+    id: 107,
+    name: "Hitmonchan",
+    sprite: "250px-107Hitmonchan.png"
+  },
+  {
+    id: 108,
+    name: "Lickitung",
+    sprite: "250px-108Lickitung.png"
+  },
+  {
+    id: 109,
+    name: "Koffing",
+    sprite: "250px-109Koffing.png"
+  },
+  {
+    id: 110,
+    name: "Weezing",
+    sprite: "250px-110Weezing.png"
+  },
+  {
+    id: 111,
+    name: "Rhyhorn",
+    sprite: "250px-111Rhyhorn.png"
+  },
+  {
+    id: 112,
+    name: "Rhydon",
+    sprite: "250px-112Rhydon.png"
+  },
+  {
+    id: 113,
+    name: "Chansey",
+    sprite: "250px-113Chansey.png"
+  },
+  {
+    id: 114,
+    name: "Tangela",
+    sprite: "250px-114Tangela.png"
+  },
+  {
+    id: 115,
+    name: "Kangaskhan",
+    sprite: "250px-115Kangaskhan.png"
+  },
+  {
+    id: 116,
+    name: "Horsea",
+    sprite: "250px-116Horsea.png"
+  },
+  {
+    id: 117,
+    name: "Seadra",
+    sprite: "250px-117Seadra.png"
+  },
+  {
+    id: 118,
+    name: "Goldeen",
+    sprite: "250px-118Goldeen.png"
+  },
+  {
+    id: 119,
+    name: "Seaking",
+    sprite: "250px-119Seaking.png"
+  },
+  {
+    id: 120,
+    name: "Staryu",
+    sprite: "250px-120Staryu.png"
+  },
+  {
+    id: 121,
+    name: "Starmie",
+    sprite: "250px-121Starmie.png"
+  },
+  {
+    id: 122,
+    name: "Mr. Mime",
+    sprite: "250px-122Mr._Mime.png"
+  },
+  {
+    id: 123,
+    name: "Scyther",
+    sprite: "250px-123Scyther.png"
+  },
+  {
+    id: 124,
+    name: "Jynx",
+    sprite: "250px-124Jynx.png"
+  },
+  {
+    id: 125,
+    name: "Electabuzz",
+    sprite: "250px-125Electabuzz.png"
+  },
+  {
+    id: 126,
+    name: "Magmar",
+    sprite: "250px-126Magmar.png"
+  },
+  {
+    id: 127,
+    name: "Pinsir",
+    sprite: "250px-127Pinsir.png"
+  },
+  {
+    id: 128,
+    name: "Tauros",
+    sprite: "250px-128Tauros.png"
+  },
+  {
+    id: 129,
+    name: "Magikarp",
+    sprite: "250px-129Magikarp.png"
+  },
+  {
+    id: 130,
+    name: "Gyarados",
+    sprite: "250px-130Gyarados.png"
+  },
+  {
+    id: 131,
+    name: "Lapras",
+    sprite: "250px-131Lapras.png"
+  },
+  {
+    id: 132,
+    name: "Ditto",
+    sprite: "250px-132Ditto.png"
+  },
+  {
+    id: 133,
+    name: "Eevee",
+    sprite: "250px-133Eevee.png"
+  },
+  {
+    id: 134,
+    name: "Vaporeon",
+    sprite: "250px-134Vaporeon.png"
+  },
+  {
+    id: 135,
+    name: "Jolteon",
+    sprite: "250px-135Jolteon.png"
+  },
+  {
+    id: 136,
+    name: "Flareon",
+    sprite: "250px-136Flareon.png"
+  },
+  {
+    id: 137,
+    name: "Porygon",
+    sprite: "250px-137Porygon.png"
+  },
+  {
+    id: 138,
+    name: "Omanyte",
+    sprite: "250px-138Omanyte.png"
+  },
+  {
+    id: 139,
+    name: "Omastar",
+    sprite: "250px-139Omastar.png"
+  },
+  {
+    id: 140,
+    name: "Kabuto",
+    sprite: "250px-140Kabuto.png"
+  },
+  {
+    id: 141,
+    name: "Kabutops",
+    sprite: "250px-141Kabutops.png"
+  },
+  {
+    id: 142,
+    name: "Aerodactyl",
+    sprite: "250px-142Aerodactyl.png"
+  },
+  {
+    id: 143,
+    name: "Snorlax",
+    sprite: "250px-143Snorlax.png"
+  },
+  {
+    id: 144,
+    name: "Articuno",
+    sprite: "250px-144Articuno.png"
+  },
+  {
+    id: 145,
+    name: "Zapdos",
+    sprite: "250px-145Zapdos.png"
+  },
+  {
+    id: 146,
+    name: "Moltres",
+    sprite: "250px-146Moltres.png"
+  },
+  {
+    id: 147,
+    name: "Dratini",
+    sprite: "250px-147Dratini.png"
+  },
+  {
+    id: 148,
+    name: "Dragonair",
+    sprite: "250px-148Dragonair.png"
+  },
+  {
+    id: 149,
+    name: "Dragonite",
+    sprite: "250px-149Dragonite.png"
+  },
+  {
+    id: 150,
+    name: "Mewtwo",
+    sprite: "250px-150Mewtwo.png"
+  },
+  {
+    id: 151,
+    name: "Mew",
+    sprite: "250px-151Mew.png"
+  }
+
 ];
 var pageContainer = document.getElementById('pageContainer');
 
